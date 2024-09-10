4b5dacb700bc8c2d042b1c80cd6439945887865c
Rana Darshan <darshanrana445@gmail.com>
2024-09-10 15:23:50 +0100

    Updated

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -3,14 +3,14 @@ describe('SwagLabs', function () {
   beforeEach('URL nevigation', function () {
     cy.visit('https://www.saucedemo.com/v1/')
 
-    cy.get('#user-name').type('standard_user').should('have.value', 'standard_user')
-    cy.get('#password').type('secret_sauce').should('have.value', 'secret_sauce')
-    cy.get('#login-button').click()
-    cy.get('.product_label').should('have.text', 'Products')
+    // cy.get('#user-name').type('standard_user').should('have.value', 'standard_user')
+    // cy.get('#password').type('secret_sauce').should('have.value', 'secret_sauce')
+    // cy.get('#login-button').click()
+    // cy.get('.product_label').should('have.text', 'Products')
 
   })
 
-  xit('Login with invalid UserID and PassWord', function () {
+  it('Login with invalid UserID and PassWord', function () {
 
     cy.get('#login-button').click().wait(2000)
     cy.get('h3').contains('Username is required').should('have.text', 'Epic sadface: Username is required')
@@ -20,7 +20,7 @@ describe('SwagLabs', function () {
 
   })
 
-  it('Side Menu functionality', function () {
+  xit('Side Menu functionality', function () {
 
     cy.get('.bm-burger-button').click()
     cy.get('#inventory_sidebar_link').should('have.text', 'All Items').click()
@@ -31,7 +31,7 @@ describe('SwagLabs', function () {
 
   })
 
-  it('Add to Cart and cart icon functionality', function () {
+  xit('Add to Cart and cart icon functionality', function () {
 
     //way 1 ----------------------------------------------------
     // cy.get('.inventory_item').each(($el, index) => {
@@ -71,7 +71,7 @@ describe('SwagLabs', function () {
 
           if (productsToHandle.some(product => labelText.includes(product))) {
             cy.get('.pricebar').should('contain', '$')
-            cy.get('button.btn_primary.btn_inventory').contains('ADD TO CART').click()
+            cy.get('button.btn_primary.btn_inventory').contains('ADD TO CART').click().wait(3000)
           }
         })
       })
@@ -90,20 +90,29 @@ describe('SwagLabs', function () {
           const labelText = $label.text()
 
           if (productstoRemove.some(product => labelText.includes(product))) {
-            cy.get('.item_pricebar').find('button').contains('REMOVE').click()
-            // cy.get('.btn_secondary cart_button').contains('REMOVE').click()
+            cy.get('.item_pricebar').find('button').contains('REMOVE').click().wait(3000)
+           // cy.get('.btn_secondary cart_button').contains('REMOVE').click()
 
           }
         })
       })
     })
 
-     cy.get('.cart_footer').find('a').contains('CHECKOUT').click()
+     cy.get('.cart_footer').find('a').contains('CHECKOUT').click().wait(3000)
      cy.get('.subheader').should('have.text','Checkout: Your Information')
+     cy.get('input[type="submit"]').click().wait(3000)
+     cy.get('h3[data-test="error"]').should('have.text','Error: First Name is required').wait(3000)
+     cy.get('#first-name').type('Test User 23').should('have.value','Test User 23')
+     cy.get('input[type="submit"]').click().wait(3000)
+     cy.get('h3[data-test="error"]').should('have.text','Error: Last Name is required')
+     cy.get('#last-name').type('Cypress').should('have.value','Cypress').wait(3000)
+     cy.get('input[type="submit"]').click().wait(3000)
+     cy.get('h3[data-test="error"]').should('have.text','Error: Postal Code is required')
+     cy.get('#postal-code').type('548411').should('have.value','548411').wait(3000)
      cy.get('input[type="submit"]').click()
-    //  cy.get('h3[@data-test="error"]').should('have.text','Error: First Name is required')
-
-  })
+     cy.get('.summary_info_label').contains('Payment Information:').should('have.text','Payment Information:')
+     cy.get('.summary_value_label').should('have.text','SauceCard #')
+   })
 })
 
 
