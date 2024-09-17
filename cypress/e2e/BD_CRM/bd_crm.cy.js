76cba926a67305f8f7f435abbeaaa5d32e9f8a20
Rana Darshan <darshanrana445@gmail.com>
2024-09-17 12:08:45 +0100

    test

--- cypress/e2e/BD_CRM/bd_crm.cy.js
+++ cypress/e2e/BD_CRM/bd_crm.cy.js
@@ -1,19 +1,24 @@
-describe('Admin User', function(){
+describe('Admin User', function () {
 
-  it('Login',function(){
+  beforeEach('Login', function () {
 
-      cy.visit('http://192.168.0.28:9000/')
-      Cypress.on('uncaught:exception', (err, runnable) => {
-        // returning false here prevents Cypress from failing the test
-        if (err.message.includes('NotificationDataApi is not defined')) {
-          return false;
-        }
-        // still throw the error if it's not the one we want to ignore
-        throw err;
-      });
+    cy.visit('http://192.168.0.28:9000/')
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from failing the test
+      if (err.message.includes('NotificationDataApi is not defined')) {
+        return false;
+      }
+      // still throw the error if it's not the one we want to ignore
+      throw err;
+    })
+
+    it('Admin Login', function () {
       cy.get('#username').type('ADM')
       cy.get('#password').type('123456')
       cy.get('button[type="submit"]').click()
       cy.url('http://192.168.0.28:9000/Dashboard/Index')
+      cy.get('.header-icon d-flex align-items-center justify-content-center ml-2').click()
+    })
+
   })
 })
