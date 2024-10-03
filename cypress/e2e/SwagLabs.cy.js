d0e411c209331d7232cd0017447bb0a6a7c90737
Rana Darshan <darshanrana445@gmail.com>
2024-10-03 16:24:12 +0100

    Test

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -3,10 +3,10 @@ describe('SwagLabs', function () {
   beforeEach('URL nevigation', function () {
     cy.visit('https://www.saucedemo.com/v1/')
 
-    // cy.get('#user-name').type('standard_user').should('have.value', 'standard_user')
-    // cy.get('#password').type('secret_sauce').should('have.value', 'secret_sauce')
-    // cy.get('#login-button').click()
-    // cy.get('.product_label').should('have.text', 'Products')
+    cy.get('#user-name').type('standard_user').should('have.value', 'standard_user')
+    cy.get('#password').type('secret_sauce').should('have.value', 'secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('.product_label').should('have.text', 'Products')
 
   })
 
